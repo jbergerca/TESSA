44ed47bfb5ddf9f91e89d08786d9431dc757cdc7
jbergerca <jberger@ucsd.edu>
2024-12-05 14:36:59 -0800

    Add files via upload

---
+++ multiplexml2sacpz.py
@@ -0,0 +1,36 @@
+import os
+import glob
+import shutil
+from obspy import read_inventory
+
+xmlFiles = glob.glob("./*.xml")
+
+for xmlFile in xmlFiles:
+    print(xmlFile)
+    inventory = read_inventory(xmlFile)
+    inventory.write("temp.txt",
+                    format="SACPZ")
+
+    file = open("temp.txt", "r")
+    data = file.readlines()
+    file.close()
+
+    total = [i for i in range(len(data)) if (data[i] == '* **************************************************\n' and data[i - 1] == '\n')]
+    if (os.path.isdir("./pazdir") == False):
+        os.mkdir("./pazdir")
+
+    name = []
+    for idx in total:
+        net = data[idx + 1].split()[-1]
+        stat = data[idx + 2].split()[-1]
+        loc = data[idx + 3].split()[-1] if data[idx + 3].split()[-1] != ":" else ""
+        chan = data[idx + 4].split()[-1]
+        name.append(f"PZ_{net}_{stat}_{loc}_{chan}")
+
+    for i in range(len(total)):
+        file = open(f"./pazdir/{name[i]}", "w")
+        new = "".join(data[total[i]:total[i+1]]) if i != (len(total) - 1) else "".join(data[total[i]::])
+        file.write(new)
+        file.close()
+
+    os.remove("temp.txt")
